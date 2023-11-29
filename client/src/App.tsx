b162c2d88298f6b3ac99cf53a1df44bb02ca4e7e
macbook <macbook@MacBook-Pro-mac.local>
2023-11-29 23:44:41 +0200

    add client test fetch request

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+
+import logo from "./logo.svg";
+
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    async function load() {
+      try {
+        const response = await fetch("http://127.0.0.1:8080/");
+        console.log("🚀 *** file: App.tsx:12 *** load *** response:", response);
+      } catch (error) {
+        console.log("🚀 *** file: App.tsx:15 *** load *** error:", error);
+      }
+    }
+
+    load();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
