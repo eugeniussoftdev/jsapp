2d96f692b08183a683ad9f39b97a0b8aae14b8ee
macbook <macbook@MacBook-Pro-mac.local>
2023-12-27 21:55:21 +0200

    todos

--- client/src/components/Todos.tsx
+++ client/src/components/Todos.tsx
@@ -7,7 +7,33 @@ import { useTodosStore } from "../store/store";
 
 export const Todos = () => {
   const todos = useTodosStore(({ todos }) => todos);
+  const addTodod = useTodosStore(({ addTodo }) => addTodo);
   const { loading, error, data } = useQuery(GET_TODOS);
   console.log("*** TODODS", todos, error, data, loading);
-  return <div>Todos</div>;
+  return (
+    <div>
+      Todos
+      <div>
+        <input
+          type="text"
+          onKeyDown={(event) => {
+            if (event.key === "Enter" && event.currentTarget.value.trim()) {
+              addTodod(event.currentTarget.value.trim());
+              event.currentTarget.value = "";
+            }
+          }}
+        />
+      </div>
+      <div>
+        {data.todos.map((todo: any) => {
+          return (
+            <div key={todo.id}>
+              <span>{todo.text}</span>
+              <span>{JSON.stringify(todo.completed)}</span>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
